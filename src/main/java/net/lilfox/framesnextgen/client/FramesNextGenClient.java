233b37fabb5a0cd3b3ce83069d33988a5a8f4dd4
lil-fox <developed.by.fox@gmail.com>
2024-01-17 13:25:47 +0100

    Initial commit

---
+++ src/main/java/net/lilfox/framesnextgen/client/FramesNextGenClient.java
@@ -0,0 +1,10 @@
+package net.lilfox.framesnextgen.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FramesNextGenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
