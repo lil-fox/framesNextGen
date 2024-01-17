be0b49e34b1a80b5ffd16daa849711bf2054129c
lil-fox <developed.by.fox@gmail.com>
2024-01-17 13:42:24 +0100

    Fix magiclib gui

---
+++ src/main/java/net/lilfox/framesnextgen/mixin/magiclibfix/GuiBaseMixin.java
@@ -0,0 +1,45 @@
+package net.lilfox.framesnextgen.mixin.magiclibfix;
+
+import fi.dy.masa.malilib.gui.GuiBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(
+        value = {GuiBase.class},
+        remap = false
+)
+public class GuiBaseMixin {
+    @Shadow
+    private int keyInputCount;
+    @Unique
+    private long openTime;
+
+    @Inject(
+            method = {"init"},
+            at = {@At("HEAD")},
+            remap = true
+    )
+    private void setOpenTimeToCloseFuckingSearchBarOnNextGuiOpening(CallbackInfo ci) {
+        this.openTime = System.nanoTime();
+        this.keyInputCount = 0;
+    }
+
+    @Inject(
+            method = {"charTyped"},
+            at = {@At("HEAD")},
+            cancellable = true,
+            remap = true
+    )
+    private void closeFuckingSearchBarOnNextGuiOpening(char charIn, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        if (this.keyInputCount <= 0 && System.nanoTime() - this.openTime <= 100000000L) {
+            ++this.keyInputCount;
+            cir.setReturnValue(true);
+        }
+
+    }
+}
